5c5eab7fa07de6a1431089f379a260149c728f06
LeeHanYeong <dev@azelf.com>
2018-03-13 12:24:15 +0900

    STATIC_URL, MEDIA_URL을 base로 이동

--- app/config/settings/local.py
+++ app/config/settings/local.py
@@ -7,11 +7,6 @@ INSTALLED_APPS += [
     'django_extensions',
 ]
 
-STATIC_ROOT = os.path.join(ROOT_DIR, '.static')
-MEDIA_ROOT = os.path.join(ROOT_DIR, '.media')
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 DATABASES = {
