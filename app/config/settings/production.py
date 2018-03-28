efc26203ba6e0c05f2ccf4bcf2d875b1ae51c08b
LeeHanYeong <dev@azelf.com>
2018-03-28 14:33:02 +0900

    pikachu도메인 추가

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -8,6 +8,7 @@ ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
     '.elasticbeanstalk.com',
+    '.pikachu.kr',
 ]
 WSGI_APPLICATION = 'config.wsgi.production.application'
 INSTALLED_APPS += [
